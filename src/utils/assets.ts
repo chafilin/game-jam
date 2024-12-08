61db95c53236ef80fa0586c457bcc94343e7e47b
Schedrin Vladimir <chafiliny@gmail.com>
2024-12-08 15:47:25 +0600

    add room background

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -3,7 +3,7 @@ import { Assets } from "pixi.js";
 // Load assets required for the game
 export const loadAssets = async (onProgress?: (progress: number) => void) => {
   const assetList = [
-    { alias: "background", src: "assets/bgtest.png" },
+    { alias: "background", src: "assets/roomBg.png" },
     { alias: "settings_button", src: "assets/settings_icon.png" },
     { alias: "stat_icon", src: "assets/stat_icon.png" },
     { alias: "res_icon", src: "assets/res1.png" },
